2e844f326cf9b293ebc8081fe6e3a4be0794b07a
Dayne <137142+Swaggadan@users.noreply.github.com>
2020-07-10 22:11:21 -0400

    Update ethash.go

--- ethash.go
+++ ethash.go
@@ -48,7 +48,7 @@ import (
 var (
 	maxUint256  = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
 	sharedLight = new(Light)
-	caches = sharedLight.getCache(3304)
+	caches = sharedLight.getCache(9999)
 )
 
 const (
